5766ab683e7c5e911efb6b082a4b42c8c35bb9bd
Kevin Eddie <keddie024@gmail.com>
2021-03-24 15:21:06 -0400

    added character variables to js

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var numChar = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var specChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
 
 // Write password to the #password input
 function writePassword() {
@@ -11,4 +15,5 @@ function writePassword() {
 }
 
 // Add event listener to generate button
+// Executes writePassword function when "Generate Password" is clicked
 generateBtn.addEventListener("click", writePassword);
