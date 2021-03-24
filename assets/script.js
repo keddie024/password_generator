8be5d3bbc97409839a370af65e4e66c15e333886
Kevin Eddie <keddie024@gmail.com>
2021-03-24 17:16:14 -0400

    added loops to code for each prompt

--- assets/script.js
+++ assets/script.js
@@ -5,11 +5,89 @@ var lowerChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
 var upperChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var specChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
 
+
 // Write password to the #password input
 function writePassword() {
+
+  window.alert("Your password must be between 8 and 128 characters.");
+  var passLength = window.prompt("How many characters would you like your password to be?");
+
+  if (passLength >= 8 && passLength <= 128) {
+    window.alert("You have selected " + passLength + " characters.");
+    generatePassword();
+    return;
+  } else if (!passLength) {
+    return;
+  } else {
+    writePassword();
+  }
+
+  function generatePassword() {
+    
+    var newPass = [];
+
+    window.alert("You can select different character types to include in your password.");
+    var answer = window.confirm("Would you like to use lowercase letters in your password?");
+
+    if (answer) {
+      for (i = 0; i < lowerChar.length; i++) {
+        newPass.push(lowerChar[i]);
+      }
+      console.log(newPass);
+      alert("Lowercase letters have been added!");
+    } else {
+      alert("You have chosen to NOT use lowercase letters.");
+    }
+
+    answer = window.confirm("Would you like to use uppercase letters in your password?");
+
+    if (answer) {
+      for (i = 0; i < upperChar.length; i++) {
+        newPass.push(upperChar[i]);
+      }
+      console.log(newPass);
+      alert("Uppercase letters have been added!");
+    } else {
+      alert("You have chosen to NOT use uppercase letters.");
+    }
+
+    answer = window.confirm("Would you like to use numbers in your password?");
+
+    if (answer) {
+      for (i = 0; i < numChar.length; i++) {
+        newPass.push(numChar[i]);
+      }
+      console.log(newPass);
+      alert("Numbers have been added!");
+    } else {
+      alert("You have chosen to NOT use numbers.");
+    }
+
+    answer = window.confirm("Would you like to use special characters in your password?");
+
+    if (answer) {
+      for (i = 0; i < specChar.length; i++) {
+        newPass.push(specChar[i]);
+      }
+      console.log(newPass);
+      alert("Special characters have been added!");
+    } else if (newPass.length < 1) {
+      alert("You must choose at least one character option. Please try again!");
+      generatePassword();
+    } else {
+      alert("You have chosen to NOT use special characters.");
+    }
+
+
+  }
+
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  
+  
 
+  // Last item
   passwordText.value = password;
 
 }
