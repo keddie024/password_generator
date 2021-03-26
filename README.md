9815bd45f3df530287b9c9431aeceb0a628c0f4c
Kevin Eddie <keddie024@gmail.com>
2021-03-26 16:56:05 -0400

    readme completed and code commented

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # password_generator
+
+## 3/26/21
+
+This application is able to generate a random password based on characters approved by the user. Upon pressing the "Generate Password" button, the user is first prompted to choose a password length between 8 and 128 characters. If this criteria is not met, the display area will say "Please try again" and the user will need to start the process again.
+
+Once the password length has been established, the user will see a series of confirmation messages which give them the option to add different types of characters to their password. If no character types are chosen, the display area will say "Please try again" and the user will need to start the process again.
+
+When the length of the password and the character types have been confirmed, the new password will appear in the text area.
+
+## Link to Application
+
+https://keddie024.github.io/password_generator/
+
+## Screenshot
+
+![Example](/assets/images/example.png)
--- assets/script.js
+++ assets/script.js
@@ -17,9 +17,11 @@ function writePassword() {
 
 function generatePassword() {
     
+  // The user is asked to input the number of characters for thei password.
   window.alert("Your password must be between 8 and 128 characters.");
   var passLength = window.prompt("How many characters would you like your password to be?");
 
+  // Checks to see if the password length matches the criteria.
   if (passLength >= 8 && passLength <= 128) {
     window.alert("You have selected " + passLength + " characters.");
   }  else {
@@ -28,9 +30,11 @@ function generatePassword() {
 
    var newPass = [];
 
+  // The user is asked if they want lowercase characters.
   window.alert("You can select different character types to include in your password.");
   var answer = window.confirm("Would you like to use lowercase letters in your password?");
 
+  // Adds characters to the pool if selected.
   if (answer) {
     for (i = 0; i < lowerChar.length; i++) {
       newPass.push(lowerChar[i]);
@@ -41,8 +45,10 @@ function generatePassword() {
     window.alert("You have chosen to NOT use lowercase letters.");
   }
 
+  // The user is asked if they want uppercase characters.
   answer = window.confirm("Would you like to use uppercase letters in your password?");
 
+  // Adds characters to the pool if selected.
   if (answer) {
     for (i = 0; i < upperChar.length; i++) {
       newPass.push(upperChar[i]);
@@ -53,8 +59,10 @@ function generatePassword() {
     window.alert("You have chosen to NOT use uppercase letters.");
   }
 
+  // The user is asked if they want number characters.
   answer = window.confirm("Would you like to use numbers in your password?");
 
+  // Adds the characters to the pool if selected.
   if (answer) {
     for (i = 0; i < numChar.length; i++) {
       newPass.push(numChar[i]);
@@ -65,8 +73,11 @@ function generatePassword() {
     window.alert("You have chosen to NOT use numbers.");
   }
 
+  // The user is asked if they want special characters.
   answer = window.confirm("Would you like to use special characters in your password?");
 
+  // Adds the characters to the pool if selected.
+  // If none have been selected, user is told to try again.
   if (answer) {
     for (i = 0; i < specChar.length; i++) {
       newPass.push(specChar[i]);
@@ -75,19 +86,22 @@ function generatePassword() {
     window.alert("Special characters have been added!");
   } else if (newPass.length < 1) {
     window.alert("You must choose at least one character option. Please try again!");
-    generatePassword();
+    return "Please try again";
   } else {
     window.alert("You have chosen to NOT use special characters.");
   }
 
+  // The generated password.
   var passString = "";
 
+  // Generates the password one character at a time from the pool of selected characters.
   for (i = 0; i < passLength; i++) {
     var passIndex = Math.floor(Math.random() * newPass.length);
     passString = passString + newPass[passIndex];
     console.log(passString);
   }
 
+  // Sends the completed password to the "password" variable in the writePassword() function.
   return passString.toString();
 
 }
